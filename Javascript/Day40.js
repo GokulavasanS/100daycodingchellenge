de2433ed1b880c4eec53c9aa9abbeaca90e555e7
Gokulavasan <111508705+GokulavasanS@users.noreply.github.com>
2023-06-21 22:38:27 +0530

    Add files via upload

---
+++ Javascript/Day40.js
@@ -0,0 +1,10 @@
+ // Leap year checker
+
+ function isLeapyear(year){
+    if((year%4 === 0 && year%100!== 0) || year%400===0){
+        return true;
+    }else{
+        return false;
+    }
+ }
+console.log(isLeapyear(2000));
